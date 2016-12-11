6fbefeeef3e6e80deea903b01993f57e4a3aec17
Teklarit <Teklarit@yandex.ru>
2016-12-11 23:50:25 +0200

    basic main_menu, load levels

---
+++ Assets/Scripts/IngameMenuFns.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameMenuFns : MonoBehaviour {
+    private bool is_active;
+    public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+        is_active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            is_active = !is_active;
+        }
+
+        canvas.enabled = is_active;
+	}
+
+    public void BackToGameButton ()
+    {
+        is_active = false;
+    }
+
+    public void ExitToMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ Assets/Scripts/MenuFns.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFns : MonoBehaviour {
+
+	public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void Credits()
+    {
+        
+    }
+}
