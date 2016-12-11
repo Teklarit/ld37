8837f58d056fd51d1194e39a1e187b69af895912
Awpteamoose <awpteamoose@gmail.com>
2016-12-12 00:49:18 +0200

    текста

---
+++ Assets/Scripts/NodeData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Node")]
+public class Node : ScriptableObject {
+	[Multiline] public string key;
+	public AudioClip clip;
+	public Transform originalObj;
+	public Transform clearObj;
+	public bool isSecretAgent;
+	public Node[] associations;
+}
